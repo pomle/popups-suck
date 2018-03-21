9ab1bec0bac45ef8233ec326c08aed5bad319fcf
Pontus Alexander <pontus.alexander@gmail.com>
2018-03-21 10:09:14 +0100

    Add React GA.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
