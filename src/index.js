e2de7116580b62ae3114fd4bb14ec6e54ea6134c
Pontus Alexander <pontus.alexander@gmail.com>
2018-03-21 10:22:27 +0100

    Disable service worker.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
