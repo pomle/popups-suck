a9d6feb62b822c6b8a18460bc7ec36c92008e0bb
Pontus Alexander <pontus.alexander@gmail.com>
2018-03-21 10:48:38 +0100

    Modularize text blocks.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {Welcome} from './Welcome';
 import {Popup} from './Popup';
 
+import {Intro} from './Text/Intro';
+
 import './App.css';
 
 const POPUP_INTRO = 'intro';
@@ -45,42 +47,9 @@ class App extends Component {
     return (
       <div className="App">
         <Popup visible={popups[POPUP_INTRO]} onClose={this.hide(POPUP_INTRO)}>
-          <h1>Hello, there!</h1>
-
-          <p>
-            We have some information for you we would like you to read.
-          </p>
-
-          <p>
-            Please read the information below, and confirm you have understood it correctly.
-          </p>
-
-          <p>
-            Behind this popup there is going to be a few questions you must answer correctly
-            in order to enter our site. This is a test to ensure you are human. This method
-            of testing is called a <a href="https://en.wikipedia.org/wiki/CAPTCHA">Captcha</a>.
-          </p>
-
-          <p>
-            You have to read this text in order to answer them and closing this popup will void
-            your ability to answer them correctly.
-          </p>
-
-          <p>
-            When you are being asked to answer a question that involves the color yellow,
-            the correct answer is blue.
-          </p>
-
-          <p>
-            Following that question you will be asked a question that involves a number. That number
-            will be 29.x where x is the number presented in the question.
-          </p>
-
-          <p>
-            Thank you for reading.<br/>
-            Team Dominicator
-          </p>
+          <Intro/>
         </Popup>
+
         <Welcome blur={Object.keys(popups).some(key => popups[key])} />
       </div>
     );
